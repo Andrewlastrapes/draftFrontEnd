445f21a85735ecd00c28809e9cd56471d4d1ac1b
Andrew Lastrapes <Andrewlastrapes@gmail.com>
2019-02-18 16:16:40 -0500

    incorrect password bug

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -30,21 +30,19 @@ export class LoginComponent {
 
         if(!u || !p){
             this.loginError = true;
-            this.errMessage = "Please type in username and password";
+            this.errMessage = "Please enter username and password";
             return;
         }
 
         this.loginServ.login(u, p).then(res => {
           if(!res["token"]){
             this.loginError = true;
-            this.errMessage = res["message"]
+            this.errMessage = "username/password incorrect"
             console.log(this.errMessage)
             return;
           }
           this.successMessage = res["message"]
-          console.log(res)
           this.transfer = res;
-          console.log(this.transfer)
           this.auth.canActivate(this.transfer , this.transfer)
           this.router.navigate(["draft-board"], {queryParams: res})
         }
